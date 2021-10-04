e3ca93ec66405df6ef102b83e00b6722e4c88135
AneeshDhulipala <72055587+AneeshDhulipala@users.noreply.github.com>
2021-10-04 07:51:35 -0500

    Add files via upload

---
+++ WordCount.py
@@ -0,0 +1,8 @@
+introString = 'Hi,My name is Aneesh and I live in Austin,Texas'
+words = introString.split()
+print(words)
+comma = introString.split(',')
+print(comma)
+def greet(name):
+    print('hello'+ name)
+greet('Aneesh')
